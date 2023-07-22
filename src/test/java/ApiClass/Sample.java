a5643b4da985b3a618e15714d4abe925ff108802
Jansiranit <jansirani3b9@gmail.com>
2023-07-22 20:00:56 +0530

    Testcode

---
+++ src/test/java/ApiClass/Sample.java
@@ -0,0 +1,21 @@
+package ApiClass;
+
+public class Sample {
+	public void empName() {
+		System.out.println("Jansi");
+	}
+	public void empId() {
+		System.out.println("3b9");
+		
+	}
+	public void empAddress() {
+		System.out.println("chennai");
+	}
+	public static void main(String[] args) {
+		Sample sample = new Sample();
+		sample.empName();
+		sample.empId();
+		sample.empAddress();
+	}
+}
+
