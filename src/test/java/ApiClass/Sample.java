f0768fd1e4749a49f1b29c2997ae4500207b36e4
Jansiranit <jansirani3b9@gmail.com>
2023-07-22 20:42:37 +0530

    jansiUpdatedcode

--- src/test/java/ApiClass/Sample.java
+++ src/test/java/ApiClass/Sample.java
@@ -11,6 +11,15 @@ public class Sample {
 	public void empAddress() {
 		System.out.println("chennai");
 	}
+	public void empHight() {
+		System.out.println("6.2");
+	}
+	public void empRank() {
+		System.out.println("A");
+	}
+	public void empDestination() {
+		System.out.println("65");
+	}
 	public static void main(String[] args) {
 		Sample sample = new Sample();
 		sample.empName();
